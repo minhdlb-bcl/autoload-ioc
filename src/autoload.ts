7334a8930dad565a2a1e808cc806f071f41c1a81
Do Lam Binh Minh <minhdlb@blockchainlabs.asia>
2020-01-22 11:47:51 +0700

    Use nodemon

--- src/autoload.ts
+++ src/autoload.ts
@@ -6,17 +6,17 @@ import IBase from "./interfaces/IBase";
 import IRouting from "./interfaces/IRouting";
 
 export default async function autoload(app: express.Express, container: Container): Promise<void> {
-    await glob.sync("**/services/*.js").map(async (file) => {
+    await glob.sync(`${__dirname}/services/*.*(js|ts)`).map(async (file) => {
         const injectClass = await import(path.resolve(file));
         container.bind<IBase>(injectClass.default).toSelf().inSingletonScope();
     });
 
-    await glob.sync("**/controllers/*.js").map(async (file) => {
+    await glob.sync(`${__dirname}/controllers/*.*(js|ts)`).map(async (file) => {
         const injectClass = await import(path.resolve(file));
         container.bind<IBase>(injectClass.default).toSelf().inSingletonScope();
     });
 
-    await glob.sync("**/routes/*.js").map(async (file) => {
+    await glob.sync(`${__dirname}/routes/*.*(js|ts)`).map(async (file) => {
         const injectClass = await import(path.resolve(file));
         container.bind<IRouting>(injectClass.default).toSelf().inSingletonScope();
         const route: IRouting = container.resolve(injectClass.default);
